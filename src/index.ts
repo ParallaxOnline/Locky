58eca982dcb94d3c9e773dad6e5cd973e71f8652
Jonathan <54381371+axisiscool@users.noreply.github.com>
2021-06-02 01:24:56 -0400

    fix(security): removed token reference and cleaned up modules

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { Client } from "./structures/client";
 
 const client = new Client();
 
-client.login('ODIxMTg1NzkyNDc5MTk5MjUy.YFAC3Q.Z0R1n6nnQ-0adaXt9xVlteHc5qE');
+client.login(process.env.BOT_TOKEN);
